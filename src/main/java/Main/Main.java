e8f97be69db92652be1e8d2ae5a1c3a02a1759ac
IsuruUDSilva <36306466+IsuruUDSilva@users.noreply.github.com>
2021-03-06 00:12:37 +0530

    sample

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label lable = new Label("Hello World");
+        Label lable = new Label("AAthuraya");
         Scene scene = new Scene(lable);
         primaryStage.setScene(scene);
         primaryStage.show();
