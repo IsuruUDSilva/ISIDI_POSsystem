89c40176faf22b0a1dedcf10179f86093ef41603
IsuruUDSilva <isuruudsilva@gmail.com>
2021-05-09 17:24:29 +0530

    Create controller folder

---
+++ src/main/java/controllers/loginController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class loginController {
+
+    @FXML
+    private Button loginB;
+
+    @FXML
+    private Button cancelB;
+
+    @FXML
+    private TextField uNametxtField;
+
+    @FXML
+    private PasswordField pWordpwField;
+
+    private void login(){
+        if (uNametxtField.getText().equals("") && pWordpwField.getText().equals("")){
+            System.out.println("Username and Password fields are empty");
+        }
+        else if (uNametxtField.getText().equals("user") && pWordpwField.getText().equals("password")){
+            popup();
+        }
+        else {
+            System.out.println("User name or Password is wrong");
+        }
+    }
+
+    private void popup(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Access Granted");
+        alert.setHeaderText("You now login as Admin");
+
+        alert.showAndWait();
+    }
+}
