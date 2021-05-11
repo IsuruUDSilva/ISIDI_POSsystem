5f2deffaee5e08ebd293cf0317e0b4e2de1f8b86
IsuruUDSilva <isuruudsilva@gmail.com>
2021-05-11 17:44:21 +0530

    Modified login controller with db

--- src/main/java/controllers/loginController.java
+++ src/main/java/controllers/loginController.java
@@ -5,41 +5,60 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import services.EmployeeService;
+
 
 public class loginController {
 
     @FXML
-    private Button loginB;
+    private Button loginBtn;
 
     @FXML
-    private Button cancelB;
+    private Button cancelBtn;
 
     @FXML
-    private TextField uNametxtField;
+    private TextField userNameTextField;
 
     @FXML
-    private PasswordField pWordpwField;
+    private PasswordField passWordPassWordField;
 
-    public void login(){
+    public void login() {
         //TODO
         /*
         check user availability by using EmployeeService ->  getEmployeeByUserName(String userName)
         then check if the employee is active after that check the password is correct
         *** PUT EVERY MESSAGES,TITTLES ETC. TO THE Const.java ***
         */
-        if (uNametxtField.getText().equals("") && pWordpwField.getText().equals("")) {
+        EmployeeService employeeService = new EmployeeService();
+
+        String userName = userNameTextField.toString();
+
+        if (userNameTextField.getText().equals("") && passWordPassWordField.getText().equals("")) {
             System.out.println("Username and Password fields are empty");
-        } else if (uNametxtField.getText().equals("user") && pWordpwField.getText().equals("password")) {
-            popup();
+        } else if (employeeService.getEmployeeByUserName(userName) != null) {
+            if (passWordPassWordField.getText().equals("password")) {
+                popupOK();
+            } else {
+                popupWrong();
+            }
+
         } else {
-            System.out.println("User name or Password is wrong");
+            popupWrong();
         }
     }
 
-    private void popup(){
+    private void popupOK() {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Access Granted");
         alert.setHeaderText("You now login as Admin");
+
+        alert.showAndWait();
+    }
+
+    private void popupWrong() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Access denied");
+        alert.setHeaderText("Check your User Name and Password");
 
         alert.showAndWait();
     }
