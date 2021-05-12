7abad330bb302c95f61f1643824d58329cf32a45
IsuruUDSilva <isuruudsilva@gmail.com>
2021-05-13 01:19:46 +0530

    create controllers and modified fxml

--- src/main/java/controllers/loginController.java
+++ src/main/java/controllers/loginController.java
@@ -1,11 +1,16 @@
 package controllers;
 
+import entities.Employee;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import services.EmployeeService;
+import services.EmployeeServiceI;
+
+import java.util.List;
 
 
 public class loginController {
@@ -22,6 +27,8 @@ public class loginController {
     @FXML
     private PasswordField passWordPassWordField;
 
+    private EmployeeServiceI employeeServiceI = new EmployeeService();
+
     public void login() {
         //TODO
         /*
@@ -29,22 +36,18 @@ public class loginController {
         then check if the employee is active after that check the password is correct
         *** PUT EVERY MESSAGES,TITTLES ETC. TO THE Const.java ***
         */
-        EmployeeService employeeService = new EmployeeService();
+//        EmployeeService employeeService = new EmployeeService();
 
-        String userName = userNameTextField.toString();
+        String userName = userNameTextField.getText();
+        String passWord = passWordPassWordField.getText();
 
-        if (userNameTextField.getText().equals("") && passWordPassWordField.getText().equals("")) {
-            System.out.println("Username and Password fields are empty");
-        } else if (employeeService.getActiveEmployeeByUserName(userName) != null) {
-            if (passWordPassWordField.getText().equals(employeeService.getActiveEmployeeByUserName(userName).getM01Password())) {
-                popupOK();
-            } else {
-                popupWrong();
-            }
-
-        } else {
+        if (validateUser(userName,passWord)){
+            popupOK();
+        }
+        else{
             popupWrong();
         }
+
     }
 
     private void popupOK() {
@@ -62,4 +65,37 @@ public class loginController {
 
         alert.showAndWait();
     }
+
+    private boolean validateUser(String userName, String passWord){
+
+        try {
+            if(userName.isEmpty()||passWord.isEmpty()||userName.isBlank()|| passWord.isBlank()){
+                return false;
+            }
+            else if(employeeServiceI.getActiveEmployeeByUserName(userName).equals(null)){
+                return false;
+            }
+            else if (employeeServiceI.getActiveEmployeeByUserName(userName).getM01UserName().equals(userName) && employeeServiceI.getActiveEmployeeByUserName(userName).getM01Password().equals(passWord)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch(NullPointerException ex) {
+            return false;
+        }
+        catch(Exception e){
+            return false;
+        }
+
+    }
+
+    public void cancel(){
+        Platform.exit();
+        System.exit(0);
+    }
+
+
+
 }
