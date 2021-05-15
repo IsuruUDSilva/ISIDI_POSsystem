25fa3333feeaf3f275cf658acc18bca948d0b6d9
IsuruUDSilva <isuruudsilva@gmail.com>
2021-05-16 03:24:39 +0530

    Logincontroller modified

--- src/main/java/controllers/BaseController.java
+++ src/main/java/controllers/BaseController.java
@@ -1,4 +1,34 @@
 package controllers;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class BaseController {
+
+    public void popup(String title, String headerText, String status) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText + " as " + status);
+
+        alert.showAndWait();
+    }
+
+    public void popup(String title, String headerText) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+
+        alert.showAndWait();
+    }
+
+    public Stage showNextStage(Pane root, boolean isResizable) {
+        Stage primaryStage = new Stage();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(isResizable);
+        primaryStage.show();
+        return primaryStage;
+    }
 }
