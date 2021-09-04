b42cc71bbc964196aca28d3f3aba2be555c8dc31
TelaniWalakanda <64357104+TelaniWalakanda@users.noreply.github.com>
2021-09-04 23:53:43 +0530

    Order fxml modified. home controller updated

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -5,12 +5,42 @@ import javafx.scene.layout.Pane;
 import java.io.IOException;
 
 
-public class HomeController extends BaseController{
+public class HomeController extends BaseController {
 
-    public void loadUser(){
+    public void loadUser() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader();
             Pane root = fxmlLoader.load(getClass().getResource("/fxml/users.fxml").openStream());
+            showNextStage(root, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadProduct() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Pane root = fxmlLoader.load(getClass().getResource("/fxml/product.fxml").openStream());
+            showNextStage(root, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadSales() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Pane root = fxmlLoader.load(getClass().getResource("/fxml/sales.fxml").openStream());
+            showNextStage(root, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadOrder() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Pane root = fxmlLoader.load(getClass().getResource("/fxml/Order.fxml").openStream());
             showNextStage(root, true);
         } catch (IOException e) {
             e.printStackTrace();
