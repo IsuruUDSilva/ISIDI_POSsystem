980dd2a608a36861cc651ec6940ce93250d3d81b
TelaniWalakanda <64357104+TelaniWalakanda@users.noreply.github.com>
2021-09-05 10:08:47 +0530

    Controllers updated

--- src/main/java/controllers/ProductController.java
+++ src/main/java/controllers/ProductController.java
@@ -1,4 +1,17 @@
 package controllers;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
 
-public class ProductController {
+public class ProductController extends BaseController {
+
+    public void loadAddUpdateProduct() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Pane root = fxmlLoader.load(getClass().getResource("/fxml/addUpdateProduct.fxml").openStream());
+            showNextStage(root, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
